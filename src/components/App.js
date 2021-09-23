3a77aa7d4a5a5ff26dcf16f19b51150cc59e789b
raphspies <raphspies@gmail.com>
2021-09-23 16:50:38 -0700

    full dynamic render; added styling

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,11 @@
 import React, {useState, useEffect}  from 'react'
-import './App.css';
+import './styles.css';
 import Axios from 'axios'
 import Card from './Card.jsx'
 
-// const API_KEY="71b79c785e848817c3c53cf4e8fb9f3a"
 const lat="33.691359"
 const lon="-118.322756"
+// const API_KEY="71b79c785e848817c3c53cf4e8fb9f3a"
 // const URL=`http://api.weatherunlocked.com/api/forecast/${lat},${lon}?app_id=3fa93b11&app_key=${API_KEY}`
 const API_KEY=`27217e9604ac8afb647d64da44c724a2`
 const URL= `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&exclude=current,hourly,minutely,alerts&appid=${API_KEY}&units=imperial`
@@ -34,13 +34,16 @@ const [isLoaded, setIsLoaded] = useState(false)
   return (
     <div className="App">
       <header className="App-header">
+        WeatherApp
+      </header>
+      <div className="App-body">
         {/* {!isLoaded ? "Loading" : weather.Days.map((day) => {
                     return (<Card weather={day} key={day.date}/>) */}
 
-        {!isLoaded ? "Loading" : weather.daily.map((day) => {
+        {!isLoaded ? "Loading" : weather.daily.slice(0, 5).map((day) => {
           return (<Card weather={day} key={day.dt}/>)
         })}
-      </header>
+      </div>
     </div>
   );
 }
--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import './styles.css';
 
 export default function Card (props) {
   // const day = (new Date(props.weather.date)).getDay();
@@ -7,17 +8,17 @@ export default function Card (props) {
   // const icon = props.weather.Timeframes[0].icon
 
   const day = ((new Date(props.weather.dt * 1000)).toString()).slice(0, 3)
-  const tempMaxF = props.weather.temp.max;
-  const tempMinF = props.weather.temp.min;
+  const tempMaxF = Math.floor(props.weather.temp.max);
+  const tempMinF = Math.floor(props.weather.temp.min);
   const weatherIconCode = props.weather.weather[0].icon
   const weatherIcon = <img src={`http://openweathermap.org/img/wn/${weatherIconCode}@2x.png`}/>
 
   return (
-    <>
+    <div className="card">
     <p/>
-      <div className="header-weekday">{day}</div>
+      <div className="card-header-weekday">{day}</div>
       <div className="body-icon">{weatherIcon}</div>
       <div className="temperatures">{tempMaxF}° {tempMinF}°</div>
-    </>
+    </div>
   )
 }
